f43a81182f16e1661f32c1c14c683da86d517ba9
valberjr <github@jvscs.maskmy.id>
2023-12-15 10:36:00 -0300

    fixed image extension

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,8 +1,6 @@
+import localFont from 'next/font/local';
 import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react';
-
-import localFont from 'next/font/local';
 
 import { cn } from '@/lib/utils';
 
@@ -10,14 +8,14 @@ const headingFont = localFont({
   src: '../public/fonts/font.woff2',
 });
 
-const Logo = () => {
+export const Logo = () => {
   return (
     <Link href="/">
       <div
         className="hover:opacity-75 transition
       items-center gap-x-2 hidden md:flex"
       >
-        <Image src="/logo.png" alt="Logo" height={30} width={30} />
+        <Image src="/logo.svg" alt="Logo" height={30} width={30} />
         <p
           className={cn('text-lg text-neutral-700 pb-1', headingFont.className)}
         >
@@ -27,5 +25,3 @@ const Logo = () => {
     </Link>
   );
 };
-
-export default Logo;
