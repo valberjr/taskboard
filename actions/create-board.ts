6d91112a6aa8d9c871339adf89e3d7ad02e7e257
valberjr <github@jvscs.maskmy.id>
2023-12-22 13:16:13 -0300

    form validation

--- actions/create-board.ts
+++ actions/create-board.ts
@@ -2,22 +2,49 @@
 
 import { db } from '@/lib/db';
 import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 import { z } from 'zod';
 
+export type State = {
+  errors?: {
+    title?: string[];
+  };
+  message?: string | null;
+};
+
 const createBoard = z.object({
-  title: z.string(),
+  title: z.string().min(3, {
+    message: 'Minimum length of 3 letters is required',
+  }),
 });
 
-export async function create(formData: FormData) {
-  const { title } = createBoard.parse({
+export async function create(prevState: State, formData: FormData) {
+  const validatedFields = createBoard.safeParse({
     title: formData.get('title'),
   });
 
-  await db.board.create({
-    data: {
-      title,
-    },
-  });
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing fields',
+    };
+  }
+
+  const { title } = validatedFields.data;
+
+  try {
+    await db.board.create({
+      data: {
+        title,
+      },
+    });
+  } catch (error) {
+    return {
+      message: 'Database error',
+    };
+  }
 
   revalidatePath('/organization/org_2ZiY2rLfGBsy07gryLxEOIDvIWv');
+
+  redirect('/organization/org_2ZiY2rLfGBsy07gryLxEOIDvIWv');
 }
